11acf843297a9633ca3f17707961252e026bff9a
kenneth m. casey <kennethmcasey@gmail.com>
2020-09-21 16:53:00 -0400

    working shell, no html or logic

---
+++ src/app/Login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent 
+{
+ //Place Holder
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AccountCreateComponent} from 'src/app/Executive/Account-Create/account-create.component'
+import {AccountMasterComponent} from 'src/app/Executive/Account-Master/account-master.component'
+import {CustomerCreateComponent} from 'src/app/Executive/Customer-Create/customer-create.component'
+import {CustomerMasterComponent} from 'src/app/Executive/Customer-Master/customer-master.component'
+import {CustomerUpdateComponent} from 'src/app/Executive/Customer-Update/customer-update.component'
+import {LoginComponent} from 'src/app/Login/login.component'
+import {AccountDepositeComponent} from 'src/app/Teller/Account-Deposit/account-deposite.component'
+import {AccountDetailsComponent} from 'src/app/Teller/Account-Details/account-details.component'
+import {AccountStatementComponent} from 'src/app/Teller/Account-Statement/account-statement.component'
+import {AccountTransferComponent} from 'src/app/Teller/Account-Transfer/account-transfer.component'
+import {AccountWithdrawComponent} from 'src/app/Teller/Account-Withdraw/account-withdraw.component'
 
-const routes: Routes = [];
+
+const routes: Routes = 
+[
+  {path:'Login', component:LoginComponent},
+
+  {path:'Teller', component:AccountDetailsComponent},
+  {path:'Teller/Deposite/:id', component:AccountDepositeComponent},
+  {path:'Teller/Statement', component:AccountStatementComponent},
+  {path:'Teller/Transfer/:id', component:AccountTransferComponent},
+  {path:'Teller/Withdraw/:id', component:AccountWithdrawComponent},
+
+  {path:'Executive/Account', component:AccountMasterComponent},
+  {path:'Executive/Account/Create', component:AccountCreateComponent},
+  {path:'Executive/Customer', component:CustomerMasterComponent},
+  {path:'Executive/Customer/Create', component:CustomerCreateComponent},
+  {path:'Executive/Customer/Update/:id', component:CustomerUpdateComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
