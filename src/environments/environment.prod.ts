05675e6e9c12a5a64f5bed59aa23bbb3b6e23f75
DivyaPuraswani <44986232+DivyaPuraswani@users.noreply.github.com>
2020-09-21 13:44:04 -0700

    Updated environment.prod.ts under environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  apiPort: 5200,                    // web api port number
+  apiEndpoints: {
+    addCustomer: '/api/Customers/AddCustomer',        // expects Customer shaped JSON object in the body
+    getCustomers: '/api/Customers/ListCustomers',     // accepts optional query parameters: S, MinSalary=[\d*]
+    getCustomer:  '/api/Customers/GetCustomer/',      // expects integer id at end of url path, e.g. .../GetCustomer/1
+    editCustomer: '/api/Customers/EditCustomer/',     // expects Customer shaped JSON object in the body
+    deleteCustomer: '/api/Customers/DeleteCustomer/', //expects integer id at end of url path, e.g. .../DeleteCustomer/1
+    addAccount: '/api/Accounts/AddAccount',
+    getAccounts: '/api/Accounts/ListAccounts',
+    getAccount:  '/api/Accounts/GetAccount/',
+    deleteAccount: '/api/Accounts/DeleteAccount/',
+    addTransaction: '/api/Transactions/AddTransaction',
+    getTransactions: '/api/Transactions/ListTransactions'
+}
 };
