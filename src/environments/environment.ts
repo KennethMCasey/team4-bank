8913d8b1a787c99eaa7ef60ebd37fcefa95a4fe7
DivyaPuraswani <44986232+DivyaPuraswani@users.noreply.github.com>
2020-09-21 13:45:06 -0700

    Updated environment.ts under environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiPort: 5200,                    // web api port number
+  apiEndpoints: {
+    addCustomer: '/api/Customers/AddCustomer',        // expects Customer shaped JSON object in the body
+    getCustomers: '/api/Customers/ListCustomers',     // accepts optional query parameters: S, MinSalary=[\d*]
+    getCustomer:  '/api/Customers/GetCustomer/',      // expects integer id at end of url path, e.g. .../GetCustomer/1
+    editCustomer: '/api/Customers/EditCustomer/',     // expects Customer shaped JSON object in the body
+    deleteCustomer: '/api/Customers/DeleteCustomer/', //expects integer id at end of url path, e.g. .../DeleteCustomer/1
+    addAccount: '/api/Accounts/AddAccount',
+    getAccounts: '/api/Accounts/ListAccounts',
+    getAccount:  '/api/Accounts/GetAccount/',
+    deleteAccount: '/api/Accounts/DeleteAccount/',
+    addTransaction: '/api/Transactions/AddTransaction',
+    getTransactions: '/api/Transactions/ListTransactions' // expects Transactions shaped JSON object in the body
+  }
 };
 
 /*
