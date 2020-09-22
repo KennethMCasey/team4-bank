50e9dad95a23173d2cf4e256bbcd17b73ad7e27d
DivyaPuraswani <44986232+DivyaPuraswani@users.noreply.github.com>
2020-09-22 11:27:43 -0700

    Login Component Ready

---
+++ Login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent 
+{
+  emailPattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+ //Place Holder
+}
