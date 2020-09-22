bb7b3718945410f77e711123f5dc332be102f072
DivyaPuraswani <44986232+DivyaPuraswani@users.noreply.github.com>
2020-09-22 12:22:45 -0700

    Update Customer under  Executive Component

---
+++ Customer-Update/customer-update.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './customer-update.component.html',
+  styleUrls: ['./customer-update.component.css']
+})
+export class CustomerUpdateComponent 
+{
+
+  //This is where the update customer logic will sit
+}
