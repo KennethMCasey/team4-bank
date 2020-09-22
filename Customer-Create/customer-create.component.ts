cc01dc8bbc1b70f04f830db1139bba8988e6b95e
DivyaPuraswani <44986232+DivyaPuraswani@users.noreply.github.com>
2020-09-22 11:05:15 -0700

    Customer Create From Executive Component

---
+++ Customer-Create/customer-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './customer-create.component.html',
+  styleUrls: ['./customer-create.component.css']
+})
+export class CustomerCreateComponent 
+{
+  //This is where the create customer logic wil go
+}
