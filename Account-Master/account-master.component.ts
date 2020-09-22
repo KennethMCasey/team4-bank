42d6880de364ad5a0a35a2d7d256e1e216b1b4aa
DivyaPuraswani <44986232+DivyaPuraswani@users.noreply.github.com>
2020-09-22 12:27:49 -0700

    Account Master- vIew Account

---
+++ Account-Master/account-master.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './account-master.component.html',
+  styleUrls: ['./account-master.component.css']
+})
+export class AccountMasterComponent
+{
+//logic
+}
